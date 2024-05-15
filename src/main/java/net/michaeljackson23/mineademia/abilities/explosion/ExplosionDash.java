bedab2931022ed252e7c51a4e6937f6a36720dc0
BuildTools <unconfigured@null.spigotmc.org>
2024-05-14 23:31:45 -0500

    technical changes

--- src/main/java/net/michaeljackson23/mineademia/abilities/explosion/ExplosionDash.java
+++ src/main/java/net/michaeljackson23/mineademia/abilities/explosion/ExplosionDash.java
@@ -21,33 +21,13 @@ public class ExplosionDash extends AbilityBase {
 
     @Override
     protected void activate(ServerPlayerEntity player, PlayerData playerData, MinecraftServer server) {
-//        if(interval <= 1) {
-//            //player.setVelocity(PlayerAngleVector.getPlayerAngleVector(player, 1, 1, 1));
-//            player.velocityModified = true;
-//            Vec3d playerRotationVec = player.getRotationVec(1.0f);
-//            player.getServerWorld().spawnParticles(ParticleTypes.EXPLOSION, player.getX() + playerRotationVec.z, player.getY() + 0.5, player.getZ() - playerRotationVec.x,
-//                    1, 0, 0, 0, 0);
-//            player.getServerWorld().spawnParticles(ParticleTypes.EXPLOSION, player.getX() - playerRotationVec.z, player.getY() + 0.5, player.getZ() + playerRotationVec.x,
-//                    1, 0, 0, 0, 0);
-//            player.getServerWorld().playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_TOTEM_USE, SoundCategory.PLAYERS, 1f, 2f);
-//        } else if(interval >= MAX_INTERVAL) {
-//            interval = -1;
-//        } else {
-//            Vec3d playerRotationVec = player.getRotationVec(1.0f);
-//            player.getServerWorld().spawnParticles(ParticleTypes.SMOKE, player.getX() + playerRotationVec.z, player.getY() + 0.5, player.getZ() - playerRotationVec.x,
-//                    1, 0, 0, 0, 0);
-//            player.getServerWorld().spawnParticles(ParticleTypes.SMOKE, player.getX() - playerRotationVec.z, player.getY() + 0.5, player.getZ() + playerRotationVec.x,
-//                    1, 0, 0, 0, 0);
-//        }
-        Vec3d playerRotationVec = player.getRotationVec(1.0f);
-//        player.getServerWorld().spawnParticles(ParticleTypes.SMOKE, player.getX() + playerRotationVec.z, player.getY() + 0.5, player.getZ() - playerRotationVec.x,
-//                1, 0, 0, 0, 0);
-//        player.getServerWorld().spawnParticles(ParticleTypes.SMOKE, player.getX() - playerRotationVec.z, player.getY() + 0.5, player.getZ() + playerRotationVec.x,
-//                1, 0, 0, 0, 0);
-
-        player.getServerWorld().spawnParticles(ParticleTypes.SMOKE, player.getX() + playerRotationVec.z, player.getY() + 0.5, player.getZ() - playerRotationVec.x,
+        Vec3d playerRotationVec = player.getRotationVec(1.0f).normalize();
+        player.getServerWorld().spawnParticles(ParticleTypes.SMOKE,
+                player.getX() + playerRotationVec.z * 2, player.getY() + 0.5, player.getZ() - playerRotationVec.x * 2,
                 1, 0, 0, 0, 0);
-        player.getServerWorld().spawnParticles(ParticleTypes.SMOKE, player.getX() - playerRotationVec.z, player.getY() + 0.5, player.getZ() + playerRotationVec.x,
+
+        player.getServerWorld().spawnParticles(ParticleTypes.SMOKE,
+                player.getX() - playerRotationVec.z * 2, player.getY() + 0.5, player.getZ() + playerRotationVec.x * 2,
                 1, 0, 0, 0, 0);
         interval++;
     }
