6619d8a1ccbc8d352ef7a6b65f1755860c2ba4f2
JacksonK <jackmkirchner@gmail.com>
2024-06-13 22:14:39 -0500

    gui

--- src/main/java/net/michaeljackson23/mineademia/gui/vestige/VestigeGUI.java
+++ src/main/java/net/michaeljackson23/mineademia/gui/vestige/VestigeGUI.java
@@ -1,8 +1,14 @@
 package net.michaeljackson23.mineademia.gui.vestige;
 
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.michaeljackson23.mineademia.Mineademia;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -11,19 +17,46 @@ public class VestigeGUI extends Screen {
     private final int BACKGROUND_WIDTH = 192;
     private final int BACKGROUND_HEIGHT = 149;
 
+    // Button dimensions
+    private final int BUTTON_WIDTH = 100;
+    private final int BUTTON_HEIGHT = 20;
+
+    // These variables need to be initialized in the constructor because they use the screen dimensions
+    private final ButtonWidget blackwhip;
+
     public VestigeGUI(Text title) {
         super(title);
+        // Center the button within the GUI
+        int buttonX = (BACKGROUND_WIDTH - BUTTON_WIDTH) / 2;
+        int buttonY = (BACKGROUND_HEIGHT - BUTTON_HEIGHT) / 2;
+
+        blackwhip = ButtonWidget.builder(Text.literal("Blackwhip"), button -> {
+                    // Insert Function Here
+                })
+                .dimensions(buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT)
+                .tooltip(Tooltip.of(Text.literal("Select Blackwhip as your vestige quirk")))
+                .build();
     }
 
+    @Override
     protected void init() {
+        // Adjust button position to account for the position of the GUI window
+        int guiLeft = (this.width - BACKGROUND_WIDTH) / 2;
+        int guiTop = (this.height - BACKGROUND_HEIGHT) / 2;
 
+        blackwhip.setX(guiLeft + (BACKGROUND_WIDTH - BUTTON_WIDTH) / 2);
+        blackwhip.setY(guiTop + (BACKGROUND_HEIGHT - BUTTON_HEIGHT) / 2);
+
+        addDrawableChild(blackwhip);
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderInGameBackground(context);
         int i = (this.width - BACKGROUND_WIDTH) / 2;
         int j = (this.height - BACKGROUND_HEIGHT) / 2;
         drawWindow(context, i, j);
+        super.render(context, mouseX, mouseY, delta);
     }
 
     public void drawWindow(DrawContext context, int x, int y) {
@@ -32,7 +65,10 @@ public class VestigeGUI extends Screen {
 
     @Override
     public boolean shouldPause() {
-        return true;
+        return false;
     }
 
+    @Override
+    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
+    }
 }
