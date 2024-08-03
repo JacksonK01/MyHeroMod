54ef5b1438d5754f7aba3f70899eaa9274576c91
1Mangomaster1 <70587721+1Mangomaster1@users.noreply.github.com>
2024-08-03 20:21:03 +0300

    FEAT - Combine changes

--- src/main/java/net/michaeljackson23/mineademia/abilitysystem/intr/abilityset/IQuirk.java
+++
@@ -1,7 +0,0 @@
-package net.michaeljackson23.mineademia.abilitysystem.intr.abilityset;
-
-/**
- * A unique set of abilities that define a quirk
- */
-public interface IQuirk extends IAbilitySet {
-}
