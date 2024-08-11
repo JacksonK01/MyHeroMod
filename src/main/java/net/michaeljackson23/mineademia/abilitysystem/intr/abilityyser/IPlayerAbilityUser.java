1c770a366c83d04320e719e50ab09429e7a9bde2
1Mangomaster1 <70587721+1Mangomaster1@users.noreply.github.com>
2024-08-11 14:13:49 +0300

    Update IPlayerAbilityUser.java

--- src/main/java/net/michaeljackson23/mineademia/abilitysystem/intr/abilityyser/IPlayerAbilityUser.java
+++ src/main/java/net/michaeljackson23/mineademia/abilitysystem/intr/abilityyser/IPlayerAbilityUser.java
@@ -4,6 +4,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import net.minecraft.util.Identifier;
+
 public interface IPlayerAbilityUser extends IAbilityUser {
 
     @Override
