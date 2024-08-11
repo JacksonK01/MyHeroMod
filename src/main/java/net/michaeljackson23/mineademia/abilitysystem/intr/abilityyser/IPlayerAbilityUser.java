7cd909f444a0075ce6f073f9a803f3e51ba0e0b0
1Mangomaster1 <70587721+1Mangomaster1@users.noreply.github.com>
2024-08-11 14:12:39 +0300

    Update IPlayerAbilityUser.java

--- src/main/java/net/michaeljackson23/mineademia/abilitysystem/intr/abilityyser/IPlayerAbilityUser.java
+++ src/main/java/net/michaeljackson23/mineademia/abilitysystem/intr/abilityyser/IPlayerAbilityUser.java
@@ -2,11 +2,17 @@ package net.michaeljackson23.mineademia.abilitysystem.intr.abilityyser;
 
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface IPlayerAbilityUser extends IAbilityUser {
 
     @Override
     @NotNull
     ServerPlayerEntity getEntity();
+
+    @Nullable
+    <T extends IActiveAbility> Class<T> getBoundAbility(@NotNull Identifier identifier);
+
+    <T extends IActiveAbility> void setBoundAbility(@NotNull Identifier Identifier, @NotNull IActiveAbility ability);
 
 }
