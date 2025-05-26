710669e9f24640dfcf35d00509ee6590943d7924
JacksonK <jackmkirchner@gmail.com>
2025-05-26 13:21:15 -0500

    FEAT - added light to IndiscriminateShock

--- src/main/java/net/michaeljackson23/mineademia/abilitysystem/usage/abilities/quirks/electrification/IndiscriminateShock.java
+++ src/main/java/net/michaeljackson23/mineademia/abilitysystem/usage/abilities/quirks/electrification/IndiscriminateShock.java
@@ -11,13 +11,22 @@ import net.michaeljackson23.mineademia.statuseffects.StatusEffectsRegister;
 import net.michaeljackson23.mineademia.util.AffectAll;
 import net.michaeljackson23.mineademia.util.DrawParticles;
 import net.michaeljackson23.mineademia.util.QuirkDamage;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LightBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 //TODO add sound
 public class IndiscriminateShock extends PhaseAbility implements ICooldownAbility {
@@ -39,12 +48,20 @@ public class IndiscriminateShock extends PhaseAbility implements ICooldownAbilit
     private final Cooldown cooldown;
     private final StatusEffectInstance taser = new StatusEffectInstance(StatusEffectsRegister.EFFECT_COWLING, 120, 0, true, false);
 
+    private final BlockState lightBlock;
+    private final BlockState air;
+    private final List<BlockPos> placedLight;
+
     public IndiscriminateShock(@NotNull IAbilityUser user) {
         super(user, "Indiscriminate Shock 1.3 Million Volts", "User releases a large amount of electricity to shock everyone near him.", AbilityCategory.ATTACK);
 
         cooldown = new Cooldown(COOLDOWN);
 
         setPhaseMethods(0, this::chargePhase, this::releasePhase);
+
+        lightBlock = Blocks.LIGHT.getDefaultState();
+        placedLight = new ArrayList<>();
+        air = Blocks.AIR.getDefaultState();
     }
 
     @Override
@@ -62,8 +79,14 @@ public class IndiscriminateShock extends PhaseAbility implements ICooldownAbilit
     private void chargePhase() {
         int ticks = getTicks();
 
-        DrawParticles.forWorld(getServerWorld()).spawnParticles(ModParticles.ELECTRIFICATION_PARTICLES,
+        ServerWorld world = getServerWorld();
+
+        DrawParticles.forWorld(world).spawnParticles(ModParticles.ELECTRIFICATION_PARTICLES,
                 getEntity().getPos().add(0, 1, 0), CHARGE_PARTICLE_COUNT, 0, 0, 0, -0.5f, true);
+
+        BlockPos pos = getEntity().getBlockPos();
+        world.setBlockState(pos, lightBlock);
+        placedLight.add(pos);
 
         if(ticks >= END_PHASE_CHARGE_TICKS) {
             nextPhase();
@@ -83,7 +106,21 @@ public class IndiscriminateShock extends PhaseAbility implements ICooldownAbilit
             QuirkDamage.doEmitterDamage(entity, toImpact, DAMAGE);
         });
 
-        if(ticks >= END_PHASE_RELEASE_TICKS)
+        if(ticks >= END_PHASE_RELEASE_TICKS) {
+            placedLight.forEach((pos) -> {
+                world.setBlockState(pos, air);
+            });
+            placedLight.clear();
             resetPhase();
+        }
+
+    }
+
+    @Override
+    public void onStartTick() {
+        super.onStartTick();
+        if(getPhase() >= 1) {
+            getServerWorld().setBlockState(getEntity().getBlockPos(), lightBlock);
+        }
     }
 }
